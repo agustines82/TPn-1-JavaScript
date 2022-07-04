b9dc5069cb88eb4505d69d4073c1bc965ebf6079
Agustin Sampayo <agustines82@gmail.com>
2022-07-04 18:20:11 -0300

    ejercicio 5 listo

---
+++ js/ejercicio5.js
@@ -0,0 +1,6 @@
+//ejercicio 5 - escribe un programa de 3 lineas que pida un numero, pida otro numero y escriba el resultado de sumar ambos
+let numeroUno = parseInt(prompt("Ingrese un numero"));
+let numeroDos = parseInt(prompt("Ingrese un segundo numero"));
+document.write(
+    "El resultado de la suma de ambos numeros es " + (numeroUno + numeroDos)
+);
