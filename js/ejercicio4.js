47fd621a198de148c2d14f892eb745dfaac4c727
Agustin Sampayo <agustines82@gmail.com>
2022-07-04 18:13:05 -0300

    ejercicio 4 listo

---
+++ js/ejercicio4.js
@@ -0,0 +1,3 @@
+//ejercicio 4 - escribe un programa de 2 lineas que pida el nombre de usurio con in prompt y escriba un texto que diga "Hola nombreUsuario"
+let nombreUsuario = prompt ("Ingrese su nombre de usuario");
+document.write("hola "+nombreUsuario);
