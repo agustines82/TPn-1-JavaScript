1fc267aee4461aa99330f3c2481d0f71ef394d6b
Agustin Sampayo <agustines82@gmail.com>
2022-07-04 19:52:47 -0300

    ejercicio 7 listo

---
+++ js/ejercicio7.js
@@ -0,0 +1,27 @@
+//Escribe un programa que pide 3 numeros y escriba en la pantalla el mayor de los 3:
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
+let numero3 = parseInt(prompt("Ingrese un tercer numero"));
+/*
+if (numero1 >= numero2) {
+    if (numero2 >= numero3) {
+        document.write("El mayor número es: " + numero1);
+    }
+} else {
+    if (numero1 <= numero2) {
+        if (numero2 <= numero3) {
+            document.write("El mayor número es: " + numero3);
+        }
+    }
+}
+*/
+//en esta opcion me funciona todo menos la opcion 7 7 9.
+
+//probando otra forma:
+if (numero1 >= numero2 && numero2 >= numero3) {
+    document.write("El mayor número es: " + numero1);
+} else {
+    if (numero1 <= numero2 && numero2 <= numero3) {
+        document.write("El mayor número es: " + numero3);
+    }
+}
