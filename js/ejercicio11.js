5c4bb120004bab6a1d48259d22c5ff4e7c0bfef1
Agustin Sampayo <agustines82@gmail.com>
2022-07-05 14:51:04 -0300

    ejercicio 11 listo y ejercicio 10 modificado

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,12 +1,14 @@
-//Escribe un programa que pida un numero y nos diga si es divisible por 2,3,5 o 7 (solo hay que comprobar si lo es por uno de los 4)
 let numero1 = parseInt(prompt("Ingrese un numero"));
 
 if (numero1 % 2 === 0) {
     document.write("El número: " + numero1 + " es divisible en 2");
-} else if (numero1 % 3 === 0) {
+}
+if (numero1 % 3 === 0) {
     document.write("El número: " + numero1 + " es divisible en 3");
-} else if (numero1 % 5 === 0) {
+}
+if (numero1 % 5 === 0) {
     document.write("El número: " + numero1 + " es divisible en 5");
-} else if (numero1 % 7 === 0) {
+}
+if (numero1 % 7 === 0) {
     document.write("El número: " + numero1 + " es divisible en 7");
 }
