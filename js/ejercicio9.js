b835fa1804507b8e1975fdbb8ba4c427c32fb1cf
Agustin Sampayo <agustines82@gmail.com>
2022-07-05 18:01:28 -0300

    resolviendo el ejercicio 9 de otras formas

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,10 +1,36 @@
-//Escribe un programa que pida una frase y escriba las vocales que aparecen.
+//Escribe un programa que pida una frase y escriba las vocales que aparecen. length substring charAt
 let frase = prompt("Ingrese una frase");
 let vocales = 0;
+let voc = "";
+let vocales3 = "aeiouáéíóú";
 
+for (i of frase.toLowerCase()) {
+    if (vocales3.includes(i)) {
+        vocales++;
+        voc = voc + i;
+    }
+}
+document.write(voc + "<br>");
+document.write(vocales);
+
+/*
+//   *** 1er modo***
 for (let letra of frase) {
     if (/[aeiouáéíóúAEIOUÁÉÍÓÚ]/.test(letra)) {
         vocales++;
     }
 }
 document.write(vocales);
+
+//   ***2do modo***
+let vocales2 = "aeiouáéíóúAEIOUÁÉÍÓÚ";
+
+for (i = 0; i <= frase.length; i++) {
+    if (vocales2.indexOf(frase[i]) !== -1) {
+        vocales++;
+        voc = voc + i;
+    }
+}
+document.write(voc + "<br>");
+document.write(vocales);
+*/
